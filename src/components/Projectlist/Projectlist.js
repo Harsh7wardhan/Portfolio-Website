f7b5cd815a9089188bc5414604eafccd0678ab6c
Harshwardhan Natu <harsh07natu@gmail.com>
2023-05-19 23:11:17 +0530

    Update Projectlist.js

--- src/components/Projectlist/Projectlist.js
+++ src/components/Projectlist/Projectlist.js
@@ -2,12 +2,6 @@ import React from 'react'
 import '../../pages/style.css';
 import ProjectCard from "./ProjectCard";
 import { Container, Row, Col } from 'react-bootstrap'
-import useradminpanel from '../../Assets/Admin-Panel-Dashboard.png'
-import olximage from '../../Assets/olx-clone image.png'
-import netfliximage from '../../Assets/React-App-netflix-clone.png'
-import gridlinesbuilders from '../../Assets/Gridlinesbuilders.png'
-import charlespizza from '../../Assets/Charlestown-Pizza.png'
-import todolist from '../../Assets/todolist.png'
 import musicrecomm from '../../Assets/music 2.png'
 import restaurant from '../../Assets/rest reviews.png'
 import disease from '../../Assets/multi disease pred.png'
